e34fc984f663a32375538490b2528b257040ad13
Ghaffarysh <abdulghaffaralshamsi@gmail.com>
2025-05-05 09:34:09 +0300

    Your update message

---
+++ Al-Rowad_University_Website/Controllers/DeanshipsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Al_Rowad_University_Website.Controllers
+{
+    public class DeanshipsController : Controller
+    {
+        public IActionResult AcademicAffairs()
+        {
+            return View();
+        }
+        public IActionResult StudentAffairs()
+        {
+            return View();
+        }
+        public IActionResult QualityAssurance()
+        {
+            return View();
+        }
+        public IActionResult GeneralSecretariat()
+        {
+            return View();
+        }
+    }
+}
---
+++ Al-Rowad_University_Website/Controllers/JurnalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Al_Rowad_University_Website.Controllers
+{
+    public class JurnalController : Controller
+    {
+        public IActionResult ScientificJournal()
+        {
+            return View();
+        }
+    }
+}
